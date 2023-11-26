5a0e7c4c207565273060142647d5f95fdb47f88f
JP Barbosa <jp@jp7.com.br>
2023-11-26 19:49:57 -0300

    Improve responsiveness

--- js/legend.js
+++ js/legend.js
@@ -19,13 +19,15 @@ export function drawLegendItems(colors) {
           <rect fill="${colors[index].pattern}" />
         </svg>
       </div>
-      <div class="legend-ja">
+      <div class="legend-name">
         <ruby class="furigana">
-          <rbc>${name.ja.map(char => `<rb>${char}</rb>`).join('')}</rbc>
-          <rtc>${name.furigana.map(char => `<rt>${char}</rt>`).join('')}</rtc>
+          <div class="ja">
+            <rtc>${name.furigana.map(char => `<rt>${char}</rt>`).join('')}</rtc>
+            <rbc>${name.ja.map(char => `<rb>${char}</rb>`).join('')}</rbc>
+          </div>
+          <rtc class="annotation"><rt>${name.en}</rt></rtc>
         </ruby>
       </div>
-      <div class="legend-en">${name.en}</div>
     `);
 
     item.onclick = () => {
