88a164f91ee1219ab44e5015667aca08d5b434e3
JP Barbosa <jp@jp7.io>
2023-12-30 01:45:55 -0300

    Fix region href link

--- components/Legend/Japan.js
+++ components/Legend/Japan.js
@@ -6,12 +6,13 @@ import { colors } from '../../js/colors.js';
 import { setActiveRegion } from '../../js/map/index.js';
 import { state } from '../../js/state.js';
 import { LegendItem } from './Item.js';
+import { replaceSpecialChars } from '../../js/utils.js';
 
 const { div } = van.tags;
 
 export const JapanElm = div({ id: 'legend-japan' }, regions.map((region, index) => (
   LegendItem(region, colors[index], state.region, {
     onclick: () => setActiveRegion(region),
-    href: `#${region.name.en}`,
+    href: `#${replaceSpecialChars(region.name.en)}`,
   }
   ))));
